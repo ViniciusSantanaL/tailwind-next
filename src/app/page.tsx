0a06a97323ffcab9994e37e5b5d5cec79a40ef98
Vinicius Santana Leão da Silva <vinicius@Marcus-MacBook-Air.local>
2023-09-26 17:14:51 -0300

    feat: fix packagelock, add side bar and prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,3 @@
-
 export default function Home() {
-  return (
-    <div className="p-4 bg-slate-900 h-screen text-slate-50">
-      <h1 className='font-bold text-5xl'>Hello World!</h1>
-      <button className="bg-sky-500 px-4 py-2 rounded-md font-medium mt-4 hover:bg-sky-600 hover:text-sky-950">
-        Sign In
-      </button>
-    </div>
-  )
+  return <h1>Hello World !</h1>
 }
