37a38f1e63b4d1e1c345d16a41d75e05c55f46c6
Vinicius Santana Leão da Silva <vinicius@Marcus-MacBook-Air.local>
2023-09-27 15:17:58 -0300

    feat: finish sidebar

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,21 +1,50 @@
-import { Search } from 'lucide-react'
+import {
+  BarChart,
+  CheckSquare,
+  Cog,
+  Flag,
+  HomeIcon,
+  LifeBuoy,
+  Search,
+  SquareStack,
+  Users,
+} from 'lucide-react'
+import { Input } from '../Input'
 import { Logo } from './Logo'
-import { MainNavigation } from './MainNavigation'
+import { NavItem } from './NavItem'
+import { Profile } from './Profile'
+import { UsedSpaceWidget } from './UsedSpaceWidget'
 
 export function Sidebar() {
   return (
-    <aside className="space-y-6 border-r border-zinc-200 px-5 py-8 ">
+    <aside className="flex flex-col gap-6 border-r border-zinc-200 px-5 py-8 ">
       <Logo />
+      <Input.Root>
+        <Input.Prefix>
+          <Search className="h-5 w-5 text-zinc-500" />
+        </Input.Prefix>
+        <Input.Control />
+      </Input.Root>
+      <nav className="space-y-0.5">
+        <NavItem title="Home" icon={HomeIcon} />
+        <NavItem title="Dashboard" icon={BarChart} />
+        <NavItem title="Projects" icon={SquareStack} />
+        <NavItem title="Tasks" icon={CheckSquare} />
+        <NavItem title="Reporting" icon={Flag} />
+        <NavItem title="Users" icon={Users} />
+      </nav>
 
-      <div className="mx-1 flex w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm">
-        <Search className="h-5 w-5 text-zinc-500" />
-        <input
-          className="w-full border-0 bg-transparent p-0 text-zinc-900 placeholder-zinc-600"
-          placeholder="Search"
-        />
+      <div className="mt-auto flex flex-col gap-6">
+        <nav className="space-y-0.5">
+          <NavItem title="Home" icon={LifeBuoy} />
+          <NavItem title="Settings" icon={Cog} />
+        </nav>
+        <UsedSpaceWidget />
+
+        <div className="h-px bg-zinc-200" />
+
+        <Profile />
       </div>
-
-      <MainNavigation />
     </aside>
   )
 }
