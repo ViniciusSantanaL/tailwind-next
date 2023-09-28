0a57c61aa22d0f2c287ac539b54ed6a0a4a827fd
Vinicius Santana Leão da Silva <vinicius@Marcus-MacBook-Air.local>
2023-09-28 14:44:36 -0300

    feat: form with multiples inputs

---
+++ src/components/Form/FileInput/Control.tsx
@@ -0,0 +1,31 @@
+'use client'
+
+import { ChangeEvent, ComponentProps } from 'react'
+import { useFileInput } from './Root'
+
+export type ControlProps = ComponentProps<'input'>
+
+export function Control({ multiple = false, ...props }: ControlProps) {
+  const { id, onFileSelected } = useFileInput()
+
+  function handleFileSelected(event: ChangeEvent<HTMLInputElement>) {
+    if (!event.target.files?.length) {
+      return
+    }
+
+    const files = Array.from(event.target.files)
+
+    onFileSelected(files, multiple)
+  }
+
+  return (
+    <input
+      type="file"
+      id={id}
+      onChange={handleFileSelected}
+      className="sr-only"
+      multiple={multiple}
+      {...props}
+    />
+  )
+}
